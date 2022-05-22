ade323bc559d0fcdd86b7c1adbff3c8ab0631f18
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-05-22 16:10:00 +0100

    commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const increaseBtn = document.querySelector('.btn-increase');
 const decreaseBtn = document.querySelector('.btn-decrease');
 const completeBtn = document.querySelector('.btn-complete');
 const resetBtn = document.querySelector('.btn-reset');
+const currentValue = document.querySelector('.current-value');
 let initialValue = progress.value;
 const maxValue = progress.max;
 const resetValue = 0;
@@ -13,21 +14,27 @@ const increaseBar = () => {
     if (progress.value !== maxValue) {
         initialValue += 20;
         progress.value = initialValue;
+        currentValue.innerHTML = `<span> ${progress.value}% </span>`;
     }
 };
 
 const decreaseBar = () => {
     initialValue -= 20;
     progress.value = initialValue;
+    currentValue.innerHTML = `<span> ${progress.value}% </span>`;
 };
 
 const completeBar = () => {
     progress.value = maxValue;
+    currentValue.innerHTML = `<span> ${progress.value}% </span>`;
 };
 
 const resetBar = () => {
     progress.value = resetValue;
+    currentValue.innerHTML = `<span> ${progress.value}% </span>`;
 };
+
+
 
 
 
